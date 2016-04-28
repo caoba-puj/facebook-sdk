f61f6e678db418b04268a070cb6822debd90529b
Wilson Alzate-Calderon <wilson.alzate@gmail.com>
2016-04-27 19:43:03 -0500

    token removed

--- facebookCrawler.py
+++ facebookCrawler.py
@@ -6,7 +6,7 @@ likesFile = open('likes.txt', 'w')
 commentsFile = open('comments.txt', 'w')
 
 #To obtain the access token enter to the url: https://developers.facebook.com/tools/explorer
-accessToken = 'CAACEdEose0cBAIfbBHQPXLfCYZBdoCZCgTr6CtsjV53cXyJ368K8y7ZCDlSosmaUMw0da9kyN7MA9QSmm6ESEbZBeGgZBZCi4H7oZAvb9ZCEa7SpgLaq6AtDwDgvVCiLZC7BC0dKxrLrA4BIA85gKDLxx36BAbjwq1EePIkcmmqoyhNAeuBRc9gCg217qq09LCOZC20IHQS0wBNBsKnr1oJeGg'
+accessToken = ''
 graph = facebook.GraphAPI(access_token=accessToken, version='2.2')
 
 profile = graph.get_object("hamburguesaselcorral")
